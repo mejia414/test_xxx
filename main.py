6da85781ea6fc3bfa1056e4509d380b5eb5f7b02
GONZALO ALDEMAR MEJIA ZARATE <52844074+mejia414@users.noreply.github.com>
2023-04-12 22:57:11 +0000

    other commint

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hola mundo")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Modelo de datos para el elemento
+class Item(BaseModel):
+    id: int
+    name: str
+
+# Base de datos ficticia
+db = []
+
+# Ruta para crear un nuevo elemento
+@app.post("/items/")
+def create_item(item: Item):
+    db.append(item)
+    return item
+
+# Ruta para obtener todos los elementos
+@app.get("/items/")
+def get_items():
+    return db
